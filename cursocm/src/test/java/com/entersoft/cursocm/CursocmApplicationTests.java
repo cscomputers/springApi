37d02735396043ce5651bcbc46f4aba23f9a4f14
Antonio Carlos <contatompx@hotmail.com>
2018-04-24 01:10:15 -0300

    Projeto criado

---
+++ cursocm/src/test/java/com/entersoft/cursocm/CursocmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.entersoft.cursocm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursocmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
