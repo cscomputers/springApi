ebd057c22f2a3dca25b088b59cfb8c292da9a3b0
Antonio Carlos <contatompx@hotmail.com>
2018-04-24 19:28:27 -0300

    Testando primeira classe de dominio

--- cursocm/src/main/java/com/entersoft/cursocm/resources/CategoriaResources.java
+++ cursocm/src/main/java/com/entersoft/cursocm/resources/CategoriaResources.java
@@ -1,16 +1,29 @@
 package com.entersoft.cursocm.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.entersoft.cursocm.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar()
+	public List<Categoria> listar()
 	{
-		return "Rest OK";
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> catList = new ArrayList<>();
+		catList.add(cat1);
+		catList.add(cat2);
+		
+
+		return catList;
 	}	
 }
