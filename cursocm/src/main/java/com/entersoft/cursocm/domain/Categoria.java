8e384baae0b764aebe3b0893bb878378bf3b1de5
Antonio Carlos <contatompx@hotmail.com>
2018-04-24 20:08:21 -0300

    Banco de Dados H2, entidade Obj Relacional

--- cursocm/src/main/java/com/entersoft/cursocm/domain/Categoria.java
+++ cursocm/src/main/java/com/entersoft/cursocm/domain/Categoria.java
@@ -2,9 +2,17 @@ package com.entersoft.cursocm.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 2955094061628039917L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
