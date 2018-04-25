0978d304341b87089e4013a0ffaa7f95cb09c966
Antonio Carlos <contatompx@hotmail.com>
2018-04-24 23:53:32 -0300

    Ajustes no endpoint /categorias/id

--- cursocm/src/main/java/com/entersoft/cursocm/resources/CategoriaResources.java
+++ cursocm/src/main/java/com/entersoft/cursocm/resources/CategoriaResources.java
@@ -21,7 +21,6 @@ public class CategoriaResources {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
 		Categoria obj = service.buscar(id);
-	
-		return ResponseEntity.ok(obj);
+		return ResponseEntity.ok().body(obj);
 	}	
 }
